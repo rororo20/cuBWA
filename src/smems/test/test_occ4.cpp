cdfffb71b82380dd34c081d437de5c0f55ae4f2a
rororo20 <100340428@qq.com>
2025-02-27 06:32:12 +0000

    <chore> add subdirectory uniitest

---
+++ src/smems/test/test_occ4.cpp
@@ -0,0 +1,19 @@
+#include "FMI_search.h"
+#include "gtest/gtest.h"
+
+class BackwardTest : public ::testing::Test
+{
+protected:
+    static void SetUpTestSuite()
+    {
+        // for unittest index
+    }
+    static void TearDownTestSuite() {}
+    void SetUp() {}
+    void TearDown() {}
+};
+
+TEST_F(BackwardTest, case1)
+{
+    ;
+}
